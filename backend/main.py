fde5b4d6caf2724d0d147ae6d35b5c3f3d171ecc
stitch-lily <apoorvabhatt27@gmail.com>
2026-06-02 21:54:02 +0530

    Fix CORS: allow all HTTPS origins

--- backend/main.py
+++ backend/main.py
@@ -28,6 +28,7 @@ for _dev in ["http://localhost:5700", "http://localhost:5500", "http://127.0.0.1
 app.add_middleware(
     CORSMiddleware,
     allow_origins=allowed_origins,
+    allow_origin_regex=r"https://.*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
